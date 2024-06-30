e5ef2ccc4c989f3f4e05e2ce4e55452dbf9b3196
Boominathan <eboominathan@gmail.com>
2024-06-30 19:26:58 +0530

    message create completed

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from .models import Message
 
 # rooms = [
 #     {'id' : 1,'name' : 'Lets learn python'},
@@ -53,7 +54,7 @@ def registerPage(request):
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            login(request,user)
+            login(request, user)
             return redirect('home')
         else:
             messages.error(request, 'An error occured during registration')
@@ -76,7 +77,19 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    print(participants)
+    context = {'room': room, 'room_messages': room_messages,'participants':participants}
     return render(request, 'base/room.html', context)
 
 
