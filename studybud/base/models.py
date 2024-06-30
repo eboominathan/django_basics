02d41456c7de1b4fb1cec122923b9b5299672a52
Boominathan <eboominathan@gmail.com>
2024-06-30 07:19:24 +0530

    Update Completed

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -20,6 +20,9 @@ class Room(models.Model):
     updated = models.DateField(auto_now=True)
     created = models.DateField(auto_now_add=True)
     
+    class Meta:
+        ordering = ['-updated','-created']
+    
 
     def __str__(self):
         return self.name
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
 
@@ -6,26 +6,42 @@ from .forms import RoomForm
 #     {'id' : 1,'name' : 'Lets learn python'},
 #     {'id' : 2,'name' : 'Design with me '},
 #     {'id' : 3,'name' : 'Frontend Developers'},
-    
+
 # ]
+
 
 def home(request):
     rooms = Room.objects.all()
-    context = {'rooms':rooms}
-    return render(request,'base/home.html',context)
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request,pk):
-    room = Room.objects.get(id=pk)  
-    context = {'room':room}       
-    return render(request,'base/room.html',context)
 
-def createRoom(request):# -> tuple[Any, Literal['room_form.html'], dict]:
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+def createRoom(request):  # -> tuple[Any, Literal['room_form.html'], dict]:
     form = RoomForm()
-    if(request.method == 'POST'):
-         form=RoomForm(request.POST)
-         if form.is_valid():
-             form.save()
-             return redirect('home')
-         
-    context = {'form':form}
-    return render(request,'base/room_form.html',context)
+    if (request.method == 'POST'):
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
