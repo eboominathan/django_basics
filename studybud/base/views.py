cef7f0860af13003d174aca5400ad0e27212c75a
Boominathan <eboominathan@gmail.com>
2024-06-30 11:29:43 +0530

    Login , Logout completed

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # rooms = [
 #     {'id' : 1,'name' : 'Lets learn python'},
@@ -11,17 +14,41 @@ from .forms import RoomForm
 # ]
 
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.errors(request,'Username or Password doesnot exists')
+
+    context = {'user':user}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
 def home(request):
-    q = request.GET.get('q') if request.GET.get('q') != None  else ''
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
-        
-                                )
+
+    )
     topics = Topic.objects.all()
-    
-    context = {'rooms': rooms,'topics':topics}
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
